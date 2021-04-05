ac2d5a0d49f09185487a5da241efa6b407f0363d
alexous95 <alexandre95870@yahoo.fr>
2021-04-05 10:20:13 +0200

    Initial Commit

---
+++ SharinganAnimation/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SharinganAnimation
+//
+//  Created by Alexandre Goncalves on 05/04/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SharinganAnimation/SharinganAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  SharinganAnimationApp.swift
+//  SharinganAnimation
+//
+//  Created by Alexandre Goncalves on 05/04/2021.
+//
+
+import SwiftUI
+
+@main
+struct SharinganAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
